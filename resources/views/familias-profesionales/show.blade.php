4efc01f117cc444dc3cd8f316b1e4b4cf577df4c
Diego <2863238@alu.murciaeduca.es>
2025-11-24 09:48:53 +0100

    solucionado index y show adaptados al nuevo array

--- resources/views/familias-profesionales/show.blade.php
+++ resources/views/familias-profesionales/show.blade.php
@@ -14,26 +14,9 @@
 
             <header>
                 <h3>{{ $familia_profesional['nombre'] }}</h3>
-                <h4>Docente ID: {{$familia_profesional['docente_id']}}</h4>
+                <h4>Codigo: {{$familia_profesional['codigo']}}</h4>
             </header>
-            <p>
-                <a href="http://github.com/2DAW-CarlosIII/{{ $familia_profesional['dominio'] }}">
-                    http://github.com/2DAW-CarlosIII/{{ $familia_profesional['dominio'] }}
-                </a>
-            </p>
             <footer>
-                <ul>
-                    @foreach ($familia_profesional['metadatos'] as $indice => $metadato)
-                        <li>{{ $indice }}: {{ $metadato }}</li>
-                    @endforeach
-                </ul>
-                @if ($familia_profesional['metadatos']['calificacion'] >= 5)
-                    <p>Estado: <strong style="color: green;">Familia profesional aprobada<strong></p>
-                    <button class="btn" >Suspender familia profesional</button>
-                @elseif($familia_profesional['metadatos']['calificacion'] < 5)
-                    <p>Estado: <strong style="color: red">Familia profesional suspensa<strong></p>
-                    <button class="btn" style="background-color: rgb(46, 128, 236);">Aprobar familia profesional</button>
-                @endif
                 <p></p>
                 <ul class="actions">
                             <li><a href="{{ action([App\Http\Controllers\FamiliasProfesionalesController::class, 'getEdit'], ['id' => $id]) }}" class="button alt">Editar Familia Profesional</a></li>
