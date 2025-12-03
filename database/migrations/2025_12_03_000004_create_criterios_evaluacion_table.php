57a9a8271ac14f651e0f12d06104d6e2fbe930c8
Diego <2863238@alu.murciaeduca.es>
2025-12-03 10:41:51 +0100

    Tabla Criterior de evaluación

---
+++ database/migrations/2025_12_03_000004_create_criterios_evaluacion_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('criterios_evaluacion', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('resultado_aprendizaje_id');
+            $table->string("codigo", 50);
+            $table->string("descripcion");
+            $table->float("peso_porcentaje")->min(0)->max(100);
+            $table->integer("orden")->min(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('criterios_evaluacion');
+    }
+};
