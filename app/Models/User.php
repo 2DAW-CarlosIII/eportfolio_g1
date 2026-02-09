d08e77fd447776ed39dae6ec81407cf2be223125
Diego <2863238@alu.murciaeduca.es>
2026-02-09 09:41:52 +0100

    Modulos Tests correctos

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,8 +21,6 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'nombre',
-        'apellidos',
         'password',
     ];
 
@@ -47,5 +45,46 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function evidencias(){
+
+    }
+
+    public function modulosMatriculados(){
+        return $this->belongsToMany(ModuloFormativo::class, 'matriculas', 'estudiante_id', 'modulo_formativo_id');
+    }
+
+    public function modulosImpartidos()
+    {
+        return $this->hasMany(ModuloFormativo::class, 'docente_id', 'id');
+    }
+
+    public function esDocente(){
+        if($this->modulosImpartidos()->count() > 0){
+            return true;
+        }
+        return false;
+    }
+
+    public function esDocenteModulo(){
+        if($this->modulosImpartidos()->exists()){
+            return true;
+        }
+        return false;
+    }
+
+    public function esEstudiante(){
+        if($this->modulosMatriculados()->exists()){
+            return true;
+        }
+        return false;
+    }
+
+    public function esEstudianteModulo(ModuloFormativo $moduloFormativo){
+        if($this->modulosMatriculados()->where('modulo_formativo_id', $moduloFormativo->id)->exists()){
+            return true;
+        }
+        return false;
     }
 }
